f4630c71cc8734601414a61d280a07a261688b9a
Damien <damienlog@gmail.com>
2013-07-09 18:08:07 +0200

    added better templates with static files

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,7 @@ from posts import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>\S+)/$', views.post, name='post')
+    url(r'^(?P<id>\S+)/$', views.details, name='details')
+    url(r'^(?P<id>\S+)/upvote$', views.upvote, name='upvote')
+    url(r'^(?P<id>\S+)/downvote$', views.downvote, name='downvote')
 )
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 from posts.models import *
 
 def index(request):
 	return render(request, 'posts/index.html', {'posts': Post.objects.all()})
 
-def post(request,id):
-	return render(request, 'posts/post.html', {'post': Post.objects.get(pk=id)})
+def details(request,id):
+	return render(request, 'posts/details.html', {'post': Post.objects.get(pk=id)})
+
+@login_required
+def upvote(request,id):
+	pass
+
+@login_required
+def downvote(request,id):
+	pass
