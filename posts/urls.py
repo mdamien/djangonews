ffa95de9e270b1fd6f3ca164789422ee8ef23c03
Damien <damienlog@gmail.com>
2013-07-11 18:51:30 +0200

    Comments done (missing comment vote for now)

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^submit/$', views.submit, name='submit'),
     url(r'^(?P<id>\d+)/$', views.details, name='details'),
     url(r'^(?P<id>\d+)/upvote$', views.upvote, name='upvote'),
-    url(r'^(?P<id>\d+)/downvote$', views.downvote, name='downvote')
+    url(r'^(?P<id>\d+)/downvote$', views.downvote, name='downvote'),
+    url(r'^(?P<post_id>\d+)/comments/reply/(?P<parent_id>\d+)$', views.comment_reply, name='comment_reply'),
 )
--- posts/views.py
+++ posts/views.py
@@ -10,13 +10,20 @@ class SubmitForm(ModelForm):
         model = Post
         fields = ('title', 'link','text')
 
+class CommentReplyForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('post', 'parent','comment')
+
 def index(request):
 	posts = list(Post.objects.all())
 	posts.sort(key=Post.score,reverse=True)
 	return render(request, 'posts/index.html', {'posts': posts})
 
 def details(request,id):
-	return render(request, 'posts/details.html', {'post': Post.objects.get(pk=id)})
+	post = Post.objects.get(pk=id)
+	comments = Comment.objects.filter(post=post)
+	return render(request, 'posts/details.html', {'post': post,'comments':comments})
 
 @login_required
 def submit(request):
@@ -32,16 +39,35 @@ def submit(request):
 		formset = SubmitForm()
 	return render(request,"posts/submit.html", {"formset": formset,})
 
+#TODO: POST requests with CRSF
 @login_required
 def upvote(request,id):
 	post = Post.objects.get(pk=id)
 	vote = Vote(voter=request.user,post=post,upvote=True)
 	vote.save()
-	return render(request, 'posts/details.html', {'post': post})
+	return redirect(post)
 
 @login_required
 def downvote(request,id):
 	post = Post.objects.get(pk=id)
 	vote = Vote(voter=request.user,post=post,upvote=False)
 	vote.save()
-	return render(request, 'posts/details.html', {'post': post})
+	return redirect(post)
+
+@login_required
+def comment_reply(request,post_id,parent_id):
+	formset = ""
+	if request.method == 'POST':
+		formset = CommentReplyForm(request.POST)
+		if formset.is_valid():
+			comment = formset.save(commit=False)
+			comment.author = request.user
+			comment.save()
+			return redirect(comment.post)
+	else:
+		comment = Comment()
+		comment.post = Post.objects.get(pk=post_id)
+		if parent_id != "0":
+			comment.parent = Comment.objects.get(pk=parent_id)
+		formset = CommentReplyForm(instance=comment)
+	return render(request,"posts/comment_reply.html", {"formset": formset,})
